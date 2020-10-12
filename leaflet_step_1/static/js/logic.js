0a5773d453ede9869413d6b2cc52863ecb4db370
erinmlee97 <erinmlee97@gmailcom>
2020-10-12 12:50:56 -0400

    added inital map

---
+++ leaflet_step_1/static/js/logic.js
@@ -0,0 +1,17 @@
+var url = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php"
+
+// Creating map object
+var myMap = L.map("map", {
+    center: [38.8375, -110.8958],
+    zoom: 5
+  });
+  
+  // Adding tile layer to the map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
