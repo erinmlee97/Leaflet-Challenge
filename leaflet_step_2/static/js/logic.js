0230edf413197b1953e470e3c25f6df2c4e91be1
erinmlee97 <erinmlee97@gmailcom>
2020-10-13 09:09:15 -0400

    finished part 2

--- leaflet_step_2/static/js/logic.js
+++ leaflet_step_2/static/js/logic.js
@@ -1,6 +1,65 @@
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 var url2 = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_plates.json"
 
+// Perform a GET request to the query URL
+d3.json(url, function(data) {
+    console.log(data.features);
+    // Using the features array sent back in the API data, create a GeoJSON layer and add it to the map
+    createFeatures(data.features);
+  });
+  function createFeatures(earthquakeData){
+    function onEachFeature(feature, layer) {
+      layer.bindPopup("<hs>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+
+    function radius(mag){
+        if (mag === 0){
+            return 1;
+        }
+        return mag * 20000;
+    }
+
+    // Create function to change color based on earthquake magnitude
+    function getColor(mag) {
+    // Conditionals for magnitude
+        if (mag >= 5) {
+        return "#ea2c2c";
+        }
+        else if (mag >= 4) {
+        return "#eaa12c";
+        }
+        else if (mag >= 3) {
+        return "#eaea2c";
+        }
+        else if (mag >= 2) {
+        return "#6fea2c";
+        }
+        else if (mag >= 1) {
+        return "#2cdaea";
+        }
+        else {
+        return "#a12cea";
+        }
+};
+  
+    var earthquakes = L.geoJSON(earthquakeData, {
+        pointToLayer: function(earthquakeData, coord){
+            return L.circle(coord, {
+                opacity: 1,
+                fillOpacity: 1,
+                color: "white",
+                fillColor: getColor(earthquakeData.properties.mag),
+                radius: radius(earthquakeData.properties.mag),
+                weight: 0.5,
+                stroke: true
+            });
+        },
+        onEachFeature: onEachFeature
+    });
+  
+    createMap(earthquakes);
+  }
+
 function createMap(earthquakes){
   // Adding tile layer to the map
   var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -9,15 +68,6 @@ function createMap(earthquakes){
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/satellite-v9",
-    accessToken: API_KEY
-  })
-
-  var streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
     accessToken: API_KEY
   })
 
@@ -45,7 +95,6 @@ function createMap(earthquakes){
     // Base map layer
     var baseMap= {
         "Satellite": satellite,
-        "Streets": streets,
         "Outdoors": outdoors,
         "Light": light
     }
@@ -68,65 +117,40 @@ function createMap(earthquakes){
         collapsed: false
       }).addTo(myMap);
 
-  // Create function to change color based on earthquake magnitude
-  function getColor(mag) {
-    // Conditionals for magnitude
-    if (mag >= 5) {
-      return "#ea2c2c";
-    }
-    else if (mag >= 4) {
-      return "#eaa12c";
-    }
-    else if (mag >= 3) {
-     return "#eaea2c";
-    }
-    else if (mag >= 2) {
-      return "#6fea2c";
-    }
-    else if (mag >= 1) {
-      return "#2cdaea";
-    }
-    else {
-      return "#a12cea";
-    }
-};
-
-// Use d3 to pull data from url
-d3.json(url, function(data){
-    function styles(feature) {
-        return {
-            opacity: 1,
-            fillOpacity: 1,
-            color: "white",
-            fillColor: getColor(feature.properties.mag),
-            radius: radius(feature.properties.mag),
-            weight: 0.5,
-            stroke: true
-        }
-    }
-    // Create funtion to set radius from magnitude
-    function radius(mag){
-        if (mag === 0){
-            return 1;
-        }
-        return mag * 5;
-    }
-
-    // Get GeoJson layer, create cirlces, and add information on earthquake
-    L.geoJson(data, {
-        pointToLayer: function(feature, coord){
-            return L.circleMarker(coord);
-        },
-        style: styles,
-        onEachFeature: function(feature, layer) {
-            layer.bindPopup(`Location: ${feature.properties.place} <hr> Magnitude: ${feature.properties.mag}`)
-        }
-    }).addTo(myMap)
-
+    d3.json(url2, function(data){
+        L.geoJSON(data, {
+            style: function(){
+                return {color: "orange",
+                        fillOpacity: 0}
+            }
+        }).addTo(faultLine)
+    })
     // Create an object for the legend
     var legend =L.control({
         position: "bottomright"
     });
+
+    function getColor(mag) {
+        // Conditionals for magnitude
+            if (mag >= 5) {
+            return "#ea2c2c";
+            }
+            else if (mag >= 4) {
+            return "#eaa12c";
+            }
+            else if (mag >= 3) {
+            return "#eaea2c";
+            }
+            else if (mag >= 2) {
+            return "#6fea2c";
+            }
+            else if (mag >= 1) {
+            return "#2cdaea";
+            }
+            else {
+            return "#a12cea";
+            }
+    };
     
     // Add details for the legend
     legend.onAdd = () => {
@@ -146,5 +170,4 @@ d3.json(url, function(data){
     }
     // Add legend to map
     legend.addTo(myMap);
-});
 }
